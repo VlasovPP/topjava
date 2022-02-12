d278acb133f743b858270cd841431db937ef2c08
Власов Павел Павлович <PVlasov@blogic.ru>
2022-02-12 12:21:54 +0700

    HW02

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,64 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+@Controller
 public class MealRestController {
-    private MealService service;
+    private static final Logger log = LoggerFactory.getLogger(MealRestController.class);
 
+    private final MealService service;
+
+    public MealRestController(MealService service) {
+        this.service = service;
+    }
+
+    public Meal get(int id){
+        int userId = SecurityUtil.authUserId();
+        return service.get(id, userId);
+    }
+
+    public void delete(int id){
+        int userId = SecurityUtil.authUserId();
+        service.delete(id, userId);
+    }
+
+    public List<MealTo> getAll(){
+        int userId = SecurityUtil.authUserId();
+        return MealsUtil.getTos(service.getAll(userId), SecurityUtil.authUserCaloriesPerDay());
+    }
+
+    public Meal create(Meal meal){
+        int userId = SecurityUtil.authUserId();
+        checkNew(meal);
+        return service.create(meal, userId);
+    }
+
+    public void update(Meal meal, int id){
+        int userId = SecurityUtil.authUserId();
+        assureIdConsistent(meal, id);
+        service.update(meal, userId);
+    }
+
+    public List<MealTo> getBetween(@Nullable LocalDate startDate, @Nullable LocalTime startTime,
+                                   @Nullable LocalDate endDate, @Nullable LocalTime endTime){
+        int userId = SecurityUtil.authUserId();
+        List<Meal> mealsDateFiltered = service.getBetweenInclusive(startDate, endDate, userId);
+        return MealsUtil.getFilteredTos(mealsDateFiltered, SecurityUtil.authUserCaloriesPerDay(), startTime, endTime);
+    }
 }
